ddd89f094952ca5f0c3a92a117626e25b195f15f
saiabhiramjaini <abhiramjaini28@gmail.com>
2026-01-02 19:55:20 +0530

    feat: db and apis

--- src/app/config.py
+++ src/app/config.py
@@ -11,6 +11,8 @@ load_dotenv(ROOT_DIR / ".env")
 settings = Dynaconf(
     settings_files=["settings.toml"], 
     root_path=ROOT_DIR,
+    environments=True,
+    load_dotenv=True,
 )
 
 db_url = os.getenv("DATABASE_URL")
